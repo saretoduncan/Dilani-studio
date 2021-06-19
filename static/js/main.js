4b876ed08168d5c196ab4132a65ebf9f8c1f6fc4
sareto <duncan.moiyo@student.moringaschool.com>
2021-06-19 12:00:02 +0300

    add comments to toggle function

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,11 @@
+//toggle function to what we do section
 const txtToggle = e => {
-  const duration = 500;
-  $(document).ready(() => {
-    $(e).click(() => $(e).toggle(duration));
-  })
-}
-txtToggle(".designFig,#designPar");
-txtToggle(".devFig,#devPar");
-txtToggle(".productFig,#productPar");
+    const duration = 500;
+    $(document).ready(() => {
+      $(e).click(() => $(e).toggle(duration));
+    })
+  }
+  //....
+txtToggle(".designFig,#designPar"); //Design section
+txtToggle(".devFig,#devPar"); //Development section
+txtToggle(".productFig,#productPar"); //project management section
